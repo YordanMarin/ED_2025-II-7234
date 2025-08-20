6d19871bd44a9407bc703355a83f30b8601403eb
yordanM <yordan@gmail.com>
2025-08-20 13:56:05 -0600

    Subiendo mi avance de la primera semana lista simple

---
+++ Lista enlazada simple/Lista.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_enlazada_simple
+{
+    internal class Lista
+    {
+        private Nodo primero = null;
+
+        public void insertar(int num)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.Numero = num;
+
+            if(primero == null)
+            {
+                primero = nuevo;
+            }
+            else
+            {
+                Nodo actual = primero;
+
+                while (actual.Siguiente != null)
+                {
+                    actual = actual.Siguiente;
+                }
+                actual.Siguiente = nuevo;
+            }
+        }
+
+        public void mostrar(ListBox list)
+        {
+            Nodo actual = primero;
+
+            while(actual != null)
+            {
+                list.Items.Add(actual.Numero);
+                actual = actual.Siguiente;
+            }
+        }
+
+    }
+}
