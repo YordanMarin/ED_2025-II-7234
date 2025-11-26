de2906a0a98896b335f8eed4508c24895742e0da
yordan <yordan@gmail.com>
2025-11-26 13:28:49 -0500

    sem15 prim

---
+++ GrafoPrim/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafoPrim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Grafo g;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = new Grafo(int.Parse(textVert.Text));
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            richTextBox1.Clear();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (g != null)
+            {
+                int o = int.Parse(textOrigen.Text);
+                int d = int.Parse(textDestino.Text);
+                int c = int.Parse(textCosto.Text);
+
+                g.insertarArista(o, d, c);
+                g.imprimirArista(listBox1, o, d, c);
+                textOrigen.Clear();
+                textDestino.Clear();
+                textCosto.Clear();
+            }
+            else MessageBox.Show("Primero define el numero de vertices");
+        }
+
+        private void btnMatriz_Click(object sender, EventArgs e)
+        {
+            if (g != null)
+            {
+                richTextBox1.Clear();
+                g.imprimirMatriz(richTextBox1);
+            }
+            else MessageBox.Show("Primero define el numero de vertices");
+        }
+
+        private void btnPrim_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            g.Prim(listBox2);
+        }
+    }
+}
---
+++ GrafoPrim/Grafo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafoPrim
+{
+    internal class Grafo
+    {
+        int[,] matriz;
+        int vertices;
+
+        public Grafo(int vertices)
+        {
+            this.vertices = vertices;
+            matriz = new int[vertices, vertices];
+        }
+
+        public void imprimirMatriz(RichTextBox rich)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    rich.Text += matriz[i, j] + "\t";
+                }
+                rich.Text += "\n";
+            }
+        }
+
+        public void insertarArista(int o, int d, int c)
+        {
+            matriz[o, d] = c;
+        }
+
+        public void imprimirArista(ListBox list, int o, int d, int c)
+        {
+            list.Items.Add($"{o} | {d} | {c}");
+        }
+
+        public void Prim(ListBox list)
+        {
+            List<int[]> prim = new List<int[]>();
+            HashSet<int> visitados = new HashSet<int>();
+
+            int inicio = 0;
+            int costoTotal = 0;
+
+            visitados.Add(inicio);
+
+            while(visitados.Count() < vertices)
+            {
+                int[] aristaMinima = null;
+                int costoMinimo = int.MaxValue;
+
+                foreach(int verticeArbol in visitados)
+                {
+                    for(int i = 0;i < vertices; i++)
+                    {
+                        if (!visitados.Contains(i) && matriz[verticeArbol, i] != 0)
+                        {
+                            int costoArista = matriz[verticeArbol, i];
+                            if (costoArista < costoMinimo)
+                            {
+                                aristaMinima = new int[] { verticeArbol, i };
+                                costoMinimo = costoArista;
+                            }
+                        }
+                    }
+                }
+
+                //IMPRIMIR PRIM
+                prim.Add(aristaMinima);
+                visitados.Add(aristaMinima[1]);
+                costoTotal += costoMinimo;
+                //imprimir aristas y costo
+                list.Items.Add($"{aristaMinima[0]} | {aristaMinima[1]} | {costoMinimo}");
+            }
+            //imprimir costo total
+            list.Items.Add("Costo total = " + costoTotal);
+        }
+    }
+}
